5a197a05d4a4a9bde2aaa7b2bac668bbffb89b6c
Krishna yadav <krishnamhn009@gmail.com>
2018-02-11 22:25:07 +0530

    added stylesheet  to spreadsheet

--- MatrixTestResult/Report.cs
+++ MatrixTestResult/Report.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MatrixTestResult
 {
-   public class Report
+    public class Report
     {
         public void CreateExcelDoc(string fileName)
         {
@@ -20,6 +20,37 @@ namespace MatrixTestResult
 
                 WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
                 worksheetPart.Worksheet = new Worksheet();
+
+                // Adding style
+                WorkbookStylesPart stylePart = workbookPart.AddNewPart<WorkbookStylesPart>();
+                stylePart.Stylesheet = GenerateStylesheet();
+                stylePart.Stylesheet.Save();
+
+                // Setting up columns
+                Columns columns = new Columns(
+                        new Column // Id column
+                        {
+                            Min = 1,
+                            Max = 1,
+                            Width = 4,
+                            CustomWidth = true
+                        },
+                        new Column // Name and Birthday columns
+                        {
+                            Min = 2,
+                            Max = 3,
+                            Width = 15,
+                            CustomWidth = true
+                        },
+                        new Column // Salary column
+                        {
+                            Min = 4,
+                            Max = 4,
+                            Width = 8,
+                            CustomWidth = true
+                        });
+
+                worksheetPart.Worksheet.AppendChild(columns);
 
                 Sheets sheets = workbookPart.Workbook.AppendChild(new Sheets());
 
@@ -37,10 +68,10 @@ namespace MatrixTestResult
                 Row row = new Row();
 
                 row.Append(
-                    ConstructCell("Id", CellValues.String),
-                    ConstructCell("Name", CellValues.String),
-                    ConstructCell("Birth Date", CellValues.String),
-                    ConstructCell("Salary", CellValues.String));
+                    ConstructCell("Id", CellValues.String, 2),
+                    ConstructCell("Name", CellValues.String, 2),
+                    ConstructCell("Birth Date", CellValues.String, 2),
+                    ConstructCell("Salary", CellValues.String, 2));
 
                 // Insert the header row to the Sheet Data
                 sheetData.AppendChild(row);
@@ -51,10 +82,10 @@ namespace MatrixTestResult
                     row = new Row();
 
                     row.Append(
-                        ConstructCell(employee.Id.ToString(), CellValues.Number),
-                        ConstructCell(employee.Name, CellValues.String),
-                        ConstructCell(employee.DOB.ToString("yyyy/MM/dd"), CellValues.String),
-                        ConstructCell(employee.Salary.ToString(), CellValues.Number));
+                        ConstructCell(employee.Id.ToString(), CellValues.Number, 1),
+                        ConstructCell(employee.Name, CellValues.String, 1),
+                        ConstructCell(employee.DOB.ToString("yyyy/MM/dd"), CellValues.String, 1),
+                        ConstructCell(employee.Salary.ToString(), CellValues.Number, 1));
 
                     sheetData.AppendChild(row);
                 }
@@ -63,13 +94,58 @@ namespace MatrixTestResult
             }
         }
 
-        private Cell ConstructCell(string value, CellValues dataType)
+        private Cell ConstructCell(string value, CellValues dataType, uint styleIndex = 0)
         {
             return new Cell()
             {
                 CellValue = new CellValue(value),
-                DataType = new EnumValue<CellValues>(dataType)
+                DataType = new EnumValue<CellValues>(dataType),
+                StyleIndex = styleIndex
             };
+        }
+
+        private Stylesheet GenerateStylesheet()
+        {
+            Stylesheet styleSheet = null;
+
+            Fonts fonts = new Fonts(
+                new Font( // Index 0 - default
+                    new FontSize() { Val = 10 }
+
+                ),
+                new Font( // Index 1 - header
+                    new FontSize() { Val = 10 },
+                    new Bold(),
+                    new Color() { Rgb = "FFFFFF" }
+
+                ));
+
+            Fills fills = new Fills(
+                    new Fill(new PatternFill() { PatternType = PatternValues.None }), // Index 0 - default
+                    new Fill(new PatternFill() { PatternType = PatternValues.Gray125 }), // Index 1 - default
+                    new Fill(new PatternFill(new ForegroundColor { Rgb = new HexBinaryValue() { Value = "66666666" } })
+                    { PatternType = PatternValues.Solid }) // Index 2 - header
+                );
+
+            Borders borders = new Borders(
+                    new Border(), // index 0 default
+                    new Border( // index 1 black border
+                        new LeftBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+                        new RightBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+                        new TopBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+                        new BottomBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+                        new DiagonalBorder())
+                );
+
+            CellFormats cellFormats = new CellFormats(
+                    new CellFormat(), // default
+                    new CellFormat { FontId = 0, FillId = 0, BorderId = 1, ApplyBorder = true }, // body
+                    new CellFormat { FontId = 1, FillId = 2, BorderId = 1, ApplyFill = true } // header
+                );
+
+            styleSheet = new Stylesheet(fonts, fills, borders, cellFormats);
+
+            return styleSheet;
         }
     }
 }
